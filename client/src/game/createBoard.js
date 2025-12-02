1f64a13bc04d652d666adeb454faa82479308568
mehdiakacem <mehdiakacem20@gmail.com>
2025-12-02 20:08:04 +0100

    added a fuction to spawn new piece

--- client/src/game/createBoard.js
+++
@@ -1,4 +0,0 @@
-const createBoard = () => Array.from({ length: 20 }, () => Array(10).fill(0));
-
-let board = createBoard();
-console.table(board);
---
+++ client/src/logic/createBoard.js
@@ -0,0 +1,1 @@
+export const createBoard = () => Array.from({ length: 20 }, () => Array(10).fill(0));
