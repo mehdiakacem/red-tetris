e38a47b2eb394770af4eafc4d7ebece714112e67
mehdiakacem <mehdiakacem20@gmail.com>
2025-12-05 10:22:01 +0100

    add gravity Tick

--- client/src/logic/movePiece.js
+++ client/src/logic/movePiece.js
@@ -1,12 +1,14 @@
 import { isValidPosition } from './isValidPosition.js';
 
-export function movePiece(piece, board, direction) {
-  const newX = piece.x + direction;
+export function movePiece(piece, board, dx, dy) {
+  const newX = piece.x + dx;
+  const newY = piece.y + dy;
 
-  if (isValidPosition(piece.matrix, board, newX, piece.y)) {
+  if (isValidPosition(piece.matrix, board, newX, newY)) {
     return {
       ...piece,
       x: newX,
+      y: newY,
     };
   }
   return piece; // no change if move is invalid
